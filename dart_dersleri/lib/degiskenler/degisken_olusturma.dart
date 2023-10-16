1207042520c67a837cba4c54a6ba10253922e351
Ahmet Rıdvan Ordulu <ahmetridvanordulu@gmail.com>
2023-10-16 23:53:51 +0300

    hi

---
+++ dart_dersleri/lib/degiskenler/degisken_olusturma.dart
@@ -0,0 +1,31 @@
+void main() {
+  var ogrenciAdi = "Ahmet";
+  var ogrenciYas = 23;
+  var ogrenciBoy = 1.81;
+  var ogrenciBasharf = 'A';
+  var ogrenciDevamEdiyormu = true;
+
+  print(ogrenciAdi);
+  print(ogrenciYas);
+  print(ogrenciBoy);
+  print(ogrenciBasharf);
+  print(ogrenciDevamEdiyormu);
+
+  int urun_id = 13;
+  String urun_adi = "Kol saati";
+  int urun_adeti = 100;
+  double urun_fiyati = 149.99;
+  String urun_tedarikci = "Rolex";
+  
+  /*print("Ürünün id    : $urun_id");
+  print("Ürünün adı   : $urun_adi");
+  print("Ürünün adeti : $urun_adeti");
+  print("Ürünün fiyatı: $urun_fiyati ₺");
+  print("Ürünün tedarikçisi : $urun_tedarikci");*/
+
+  int x = 200;
+  int y = 100;
+
+  print("$x ve $y nin toplamı ${x + y} dır.");
+
+}
