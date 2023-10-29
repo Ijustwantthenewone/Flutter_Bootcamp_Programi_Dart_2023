67bbf53f66d49acc538f991e4830433e06541ee5
Ahmet Rıdvan Ordulu <ahmetridvanordulu@gmail.com>
2023-10-30 00:04:18 +0300

    a
    a

---
+++ dart_dersleri/lib/nesne_tabanli_programlama/kalitim/saray.dart
@@ -0,0 +1,8 @@
+import 'package:dart_dersleri/nesne_tabanli_programlama/kalitim/ev.dart';
+
+class Saray extends Ev{
+  int kuleSayisi;
+
+  Saray({required this.kuleSayisi}) : super(pencereSayisi: 3); //19:51
+
+}
