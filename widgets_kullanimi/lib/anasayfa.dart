34b329c1e9d5cb725d29ecdf4c13d6113849e095
Ahmet Rıdvan Ordulu <ahmetridvanordulu@gmail.com>
2024-01-04 14:09:10 +0300

    a
    a

--- widgets_kullanimi/lib/anasayfa.dart
+++ widgets_kullanimi/lib/anasayfa.dart
@@ -11,6 +11,11 @@ class _anaSayfaState extends State<anaSayfa> {
   var tfcontroller = TextEditingController();
   String alinanVeri = "";
   String resimadi = "baklava.png";
+  bool switchControl = false;
+  bool checkBoxControl = false;
+  int radioDeger = 0;
+  bool progress = false;
+  double slider = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +29,7 @@ class _anaSayfaState extends State<anaSayfa> {
               padding: const EdgeInsets.only(left: 10, right: 10),
               child: TextField(
                 controller: tfcontroller,
-                decoration: InputDecoration(hintText: "Gelen veri"),
+                decoration: const InputDecoration(hintText: "Gelen veri"),
                 keyboardType: TextInputType.number,
                 obscureText: true,
               ),
@@ -36,7 +41,7 @@ class _anaSayfaState extends State<anaSayfa> {
                     print(alinanVeri);
                   });
                 },
-                child: Text("Veriyi Al")),
+                child: const Text("Veriyi Al")),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -46,17 +51,124 @@ class _anaSayfaState extends State<anaSayfa> {
                         resimadi = "sutlac.png";
                       });
                     },
-                    child: Text("Resim 1")),
-                Image.network("http://kasimadalan.pe.hu/yemekler/resimler/$resimadi",width: 200,height: 200),
+                    child: const Text("Resim 1")),
+                Image.network(
+                    "http://kasimadalan.pe.hu/yemekler/resimler/$resimadi",
+                    width: 200,
+                    height: 200),
                 TextButton(
                     onPressed: () {
                       setState(() {
                         resimadi = "pizza.png";
                       });
                     },
-                    child: Text("Resim 2")),
+                    child: const Text("Resim 2")),
               ],
-            )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 150,
+                  child: SwitchListTile(
+                    value: switchControl,
+                    title: const Text("Dart"),
+                    onChanged: (value) {
+                      setState(() {
+                        switchControl = value;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  child: CheckboxListTile(
+                    value: checkBoxControl,
+                    title: const Text("Flutter"),
+                    onChanged: (c) {
+                      setState(() {
+                        checkBoxControl = c!;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: 200,
+                  child: RadioListTile(
+                    title: Text("İstanbul"),
+                    value: 3,
+                    groupValue: radioDeger,
+                    onChanged: (value) {
+                      setState(() {
+                        radioDeger = value!;
+                        print(radioDeger);
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 150,
+                  child: RadioListTile(
+                    title: Text("Ankara"),
+                    value: 1,
+                    groupValue: radioDeger,
+                    onChanged: (value) {
+                      setState(() {
+                        radioDeger = value!;
+                        print(radioDeger);
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                    onPressed: () {
+                      setState(() {
+                        progress = true;
+                      });
+                    },
+                    child: const Text("Başla")),
+                Visibility(
+                  visible: progress,
+                  child: CircularProgressIndicator(
+
+                    color: Colors.black,
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      setState(() {
+                        progress = false;
+                      });
+                    },
+                    child: const Text("Dur")),
+              ],
+            ),
+            Slider(value: slider, divisions: 11,max: 100,min: 0, onChanged: (value) {
+              setState(() {
+                slider = value;
+                print(value.toInt());
+              });
+            },),
+            Text("${slider.toInt()}"),
+
+            ElevatedButton(
+                onPressed: () {
+                  print(
+                      "Switch'in durumu $switchControl Checkbox'ın durumu $checkBoxControl"); //20:25
+                },
+                child: Text("Göster"))
           ],
         ),
       ),
